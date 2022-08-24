d889c9f870eb7bb4af5cfa98826138c3f1d15f4b
guilhermeg2k <guilhermen2k@gmail.com>
2022-08-24 20:02:05 -0300

    Complete vote validations

--- src/backend/controller/poolVoteController.ts
+++ src/backend/controller/poolVoteController.ts
@@ -1,7 +1,56 @@
-import poolVotesRepository from '@backend/repository/poolVoteRepository';
 import poolService from '@backend/service/poolService';
 import poolVoteService from '@backend/service/poolVoteService';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { z } from 'zod';
+
+const areVotesValid = async (poolId: any, userId: any, poolVotes: any) => {
+  const typesValidator = z.object({
+    poolId: z.string(),
+    userId: z.string(),
+    poolVotes: z.array(
+      z.object({
+        anilistId: z.number(),
+        type: z.string(),
+      })
+    ),
+  });
+
+  const { success: areTypesValid } = typesValidator.safeParse({
+    poolId,
+    userId,
+    poolVotes,
+  });
+
+  if (!areTypesValid) {
+    return false;
+  }
+
+  const pool = await poolService.get(String(poolId));
+
+  const areMultipleVotesValid = pool.multiOptions && poolVotes.length > 1;
+  if (!areMultipleVotesValid) {
+    return false;
+  }
+
+  const poolEndDate = new Date(pool.endDate);
+  const now = new Date();
+  const isVoteDateValid = poolEndDate > now;
+  if (!isVoteDateValid) {
+    return false;
+  }
+
+  const userVotes = await poolVoteService.getUserVotesOnPool(
+    String(userId),
+    String(poolId)
+  );
+  const userHasAlreadyVoted = userVotes && userVotes.length > 0;
+
+  if (userHasAlreadyVoted) {
+    return false;
+  }
+
+  return true;
+};
 
 const create = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -9,32 +58,18 @@ const create = async (req: NextApiRequest, res: NextApiResponse) => {
     const { poolVotes } = req.body;
     const { id: userId } = req.cookies;
 
-    if (userId && poolId && poolVotes) {
-      const pool = await poolService.get(String(poolId));
-
-      if (!pool.multiOptions && poolVotes.length > 1) {
-        return res.status(400).send('');
-      }
-
-      const userVotes = await poolVoteService.getUserVotesOnPool(
-        userId,
-        String(poolId)
-      );
-
-      if (userVotes && userVotes.length > 0) {
-        return res.status(400).send('');
-      }
-
-      await poolVoteService.create(
-        userId,
-        String(poolId),
-        poolVotes as Array<PoolOption>
-      );
-
-      return res.status(200).send('Votes created');
+    const areUserVotesValid = await areVotesValid(poolId, userId, poolVotes);
+    if (!areUserVotesValid) {
+      return res.status(400).send('Invalid votes');
     }
 
-    return res.status(400).send('');
+    await poolVoteService.create(
+      String(userId),
+      String(poolId),
+      poolVotes as Array<PoolOption>
+    );
+
+    return res.status(200).send('Votes created');
   } catch (error) {
     console.log(error);
     return res.status(500).send('');
