e5796a254580dc01036950c1f092b6b28a9c3da1
guilhermeg2k <guilhermen2k@gmail.com>
2022-12-21 23:10:05 -0300

    Change AWS env vars name

--- src/backend/database/index.ts
+++ src/backend/database/index.ts
@@ -1,9 +1,9 @@
 import AWS from 'aws-sdk';
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  accessKeyId: process.env.ANIPOOL_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.ANIPOOL_AWS_SECRET_ACCESS_KEY,
+  region: process.env.ANIPOOL_AWS_REGION,
 });
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
