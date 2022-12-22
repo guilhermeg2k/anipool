5b2edcf1f3df8828c023eaa311076aba9ed0583f
guilhermeg2k <guilhermen2k@gmail.com>
2022-12-21 23:22:46 -0300

    Add variable to api url

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const baseURL =
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:3000/api';
+
 const axiosClient = axios.create({
-  baseURL: `http://localhost:3000/api`,
+  baseURL,
 });
 
 export default axiosClient;
