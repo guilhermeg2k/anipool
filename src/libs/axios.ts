1f77bd60bd83fb75229a75c2420a33e90b8135ff
guilhermeg2k <guilhermen2k@gmail.com>
2022-12-21 23:36:03 -0300

    Fix CORS error on vercel

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-const baseURL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:3000/api';
-
 const axiosClient = axios.create({
-  baseURL,
+  baseURL: '/api',
 });
 
 export default axiosClient;
