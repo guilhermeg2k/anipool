8128694bd29a7d59cc921bb96ac962eaf9400cd5
guilhermeg2k <guilhermen2k@gmail.com>
2022-12-24 16:05:13 -0300

    Return 400 when trying to get an user that doesn't exists

--- src/backend/controller/userController.ts
+++ src/backend/controller/userController.ts
@@ -7,6 +7,9 @@ const getCurrentUser = async (req: NextApiRequest, res: NextApiResponse) => {
     const { userToken } = req.cookies;
     const { id } = await getTokenPayload(String(userToken));
     const user = await userService.get(id);
+    if (!user) {
+      return res.status(400).send('');
+    }
     return res.status(200).send(user);
   } catch (error) {
     return res.status(500).send('');
